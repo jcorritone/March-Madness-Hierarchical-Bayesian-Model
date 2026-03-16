c1408f39cfe6d9b4da0b2de0efacb5e35d0104b7
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 20:33:27 +0100

    Update build_matchup_training_data.R

--- R/features/build_matchup_training_data.R
+++ R/features/build_matchup_training_data.R
@@ -61,7 +61,7 @@ build_matchup_training_data <- function(tourney_results, team_season_features, s
       OffEffDiff = Team1_OffEff - Team2_OffEff,
       DefEffDiff = Team1_DefEff - Team2_DefEff,
       NetEffDiff = Team1_NetEff - Team2_NetEff,
-      AdjTempoDiff = Team1_AdjTempo - Team2_AdjTempo,
+      AdjTempoMismatch = abs(Team1_AdjTempo - Team2_AdjTempo),
       SOSDiff = Team1_SOS - Team2_SOS) %>%
     
   arrange(Season, Team1, Team2)
