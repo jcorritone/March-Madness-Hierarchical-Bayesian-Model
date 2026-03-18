0275e3ad005b192eb839e54ef5aab43d0cf3f29d
Joseph Corritone <jcorritone@gmail.com>
2026-03-18 18:36:39 +0100

    Update fit_model_03_ridge.R

--- R/models/fit_model_03_ridge.R
+++ R/models/fit_model_03_ridge.R
@@ -9,12 +9,12 @@ fit_model_03_ridge <- function(train_data_ridge) {
       RidgeRatingDiff,
       OffEffDiff,
       DefEffDiff,
-      AdjTempoDiff
-    ) %>%
+      AdjTempoDiff,
+      SOSDiff) %>%
     drop_na()
 
   glm(
-    Outcome ~ SeedDiff + RidgeRatingDiff + OffEffDiff + DefEffDiff + AdjTempoDiff,
+    Outcome ~ SeedDiff + RidgeRatingDiff + OffEffDiff + DefEffDiff + AdjTempoDiff + SOSDiff,
     data = model_data,
     family = binomial(link = "logit"))
 }
